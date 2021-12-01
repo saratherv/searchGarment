703eb2071ed6b27aa70ac8b039ce74e2e28eec8c
harshita_sarathe <rajeev.sarathe@finplex.in>
2021-12-01 16:28:53 +0530

    adding client

---
+++ client/src/components/Search.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Scroll from './Scroll';
+import SearchList from './SearchList';
+
+function Search({ details }) {
+
+  const [searchField, setSearchField] = useState("");
+  const [searchShow, setSearchShow] = useState(false);
+
+  const filteredPersons = details.filter(
+    person => {
+      return (
+        person
+        .name
+        .toLowerCase()
+        .includes(searchField.toLowerCase()) ||
+        person
+        .email
+        .toLowerCase()
+        .includes(searchField.toLowerCase())
+      );
+    }
+  );
+
+  const handleChange = e => {
+    setSearchField(e.target.value);
+    if(e.target.value===""){
+      setSearchShow(false);
+    }
+    else {
+      setSearchShow(true);
+    }
+  };
+
+  function searchList() {
+  	if (searchShow) {
+	  	return (
+	  		<Scroll>
+	  			<SearchList filteredPersons={filteredPersons} />
+	  		</Scroll>
+	  	);
+	  }
+  }
+
+  return (
+    <section className="garamond">
+			<div className="navy georgia ma0 grow">
+				<h2 className="f2">Garment Store</h2>
+			</div>
+			<div className="pa2">
+				<input 
+					className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+					type = "search" 
+					placeholder = "Search Items" 
+					onChange = {handleChange}
+				/>
+			</div>
+			{searchList()}
+		</section>
+  );
+}
+
+export default Search;
